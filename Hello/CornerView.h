7e49b2770d893081d92b682ca0fdcadd7269a936
Emil Sågfors <emil.sagfors@iki.fi>
2014-01-26 12:43:34 +0200

    many class

---
+++ Hello/CornerView.h
@@ -0,0 +1,13 @@
+//
+//  CornerView.h
+//  Hello
+//
+//  Created by Emil Sågfors on 1/26/14.
+//  Copyright (c) 2014 Emil Sågfors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CornerView : UIView
+
+@end
---
+++ Hello/CornerView.m
@@ -0,0 +1,13 @@
+//
+//  CornerView.m
+//  Hello
+//
+//  Created by Emil Sågfors on 1/26/14.
+//  Copyright (c) 2014 Emil Sågfors. All rights reserved.
+//
+
+#import "CornerView.h"
+
+@implementation CornerView
+
+@end
